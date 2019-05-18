df9a901e832fef96f822ff5e74cc78c3c8903f2f
Fabio Torchetti <mrwho@wedjaa.net>
2019-05-18 09:25:12 -0500

    Initial setup for the time validation logic.

---
+++ src/modules/errors/time-validation-error.js
@@ -0,0 +1,21 @@
+/**
+ * Custom error for time validation errors.
+ */
+export class TimeValidationError extends Error {
+    
+    /**
+     * Creates and instance of `TimeValidationError` that will
+     * be populated with the time range properties.
+     * 
+     * @constructor
+     * @param {String} startTime Provided working hours start time
+     * @param {String} endTime Provided working hours end time
+     * @param {String} errorMessage Error message to be displayed
+     */
+    constructor(startTime, endTime, errorMessage) {
+        super(errorMessage);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+}
---
+++ src/modules/time-validation.js
@@ -0,0 +1,37 @@
+import { TimeValidationError } from './errors/time-validation-error';
+
+/**
+ * This class provides the data validation logic.
+ */
+export class TimeValidation {
+
+    /**
+     * Creates an instance of the time validation class
+     * initialized to verify times between `startTime` and
+     * `endTime` as valid working hours.
+     * 
+     * @constructor
+     * @param {String} startTime 
+     *  This is the minimum allowed valid starting time.
+     * @param {String} endTime 
+     *  This is the maximum allowed ending time.
+     * @throws {@link TimeValidationError} if `startTime` is after `endTime`.
+     */
+    constructor(startTime, endTime) {
+        if (this._isGreater())
+        this._startTime = startTime;
+        this._endTime = endTime;
+    }
+
+    /**
+     * Compares `timeA` and `timeB` and
+     * returns a truthy value if `timeA` is
+     * greater than `timeB`.
+     * 
+     * @param {String} timeA First time to compare
+     * @param {String} timeB Second time to compare
+     */
+    _isGreater(timeA, timeB) {
+
+    }
+}
